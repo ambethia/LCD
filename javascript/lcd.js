6b830584fead1b179f52da46e76110bb6ba5e050
Jason L Perry <jasper@ambethia.com>
2017-03-14 17:18:19 -0400

    Slightly more modern JS

--- javascript/lcd.js
+++ javascript/lcd.js
@@ -1,68 +1,68 @@
-var TABLE = [119, 36, 93, 109, 46, 107, 123, 37, 127, 111]; // Binary notation is not supported in Js.
-var H_SYM = "-";
-var V_SYM = "|";
-var S_SYM = " ";
+const TABLE = [
+  0b1110111,
+  0b0100100,
+  0b1011101,
+  0b1101101,
+  0b0101110,
+  0b1101011,
+  0b1111011,
+  0b0100101,
+  0b1111111,
+  0b1101111
+]
 
-var LCD = function(input, segments) {
-  var numbers = [];
-  input.split("").forEach(function(c, i) {
-    numbers[i] = parseInt(c);
-  });
-  this.numbers = numbers;
-  this.segments = segments;
+const H_SYM = '-'
+const V_SYM = '|'
+const S_SYM = ' '
+
+class LCD {
+  constructor (input, segments) {
+    this.numbers = input.split('').map(c => parseInt(c))
+    this.segments = segments
+  }
+
+  test (number, position) {
+    return (TABLE[number] & (1 << position)) > 0
+  }
+
+  draw () {
+    let offset = 0
+    for (let row = 0; row < 5; row++) {
+      if (row % 2 === 0) {
+        this.numbers.forEach(number => {
+          const symbol = this.test(number, row + offset) ? H_SYM : S_SYM
+          process.stdout.write(S_SYM + symbol.repeat(this.segments) + S_SYM + S_SYM)
+        })
+        process.stdout.write('\n')
+      } else {
+        for (let i = 0; i < this.segments; i++) {
+          this.numbers.forEach(number => {
+            for (let column = 0; column < 2; column++) {
+              const symbol = this.test(number, row + column + offset) ? V_SYM : S_SYM
+              process.stdout.write(symbol + S_SYM.repeat(column === 0 ? this.segments : 1))
+            }
+          })
+          process.stdout.write('\n')
+        }
+        offset++
+      }
+    }
+  }
 }
 
-LCD.prototype.test = function(number, position) {
-  return (TABLE[number] & (1 << position)) > 0;
-};
-
-LCD.prototype.draw = function() {
-  var offset = 0;
-  for (var row = 0; row < 5; row++) {
-    if (row % 2 == 0) {
-      var self = this;
-      this.numbers.forEach(function(number) {
-        var symbol = self.test(number, row + offset) ? H_SYM : S_SYM;
-        var symbols = "";
-        for (var i = 0; i < self.segments; i++) {
-          symbols += symbol;
-        };
-        process.stdout.write(S_SYM + symbols + S_SYM + S_SYM);
-      });
-      process.stdout.write("\n");
-    } else {
-      for (var i = 0; i < this.segments; i++) {
-        var self = this;
-        this.numbers.forEach(function(number) {
-          for (var column = 0; column < 2; column++) {
-            var symbol = self.test(number, row + column + offset) ? V_SYM : S_SYM;
-            var spaces = "";
-            for (var i = 0; i < (column == 0 ? self.segments : 1); i++) {
-              spaces += S_SYM;
-            };
-            process.stdout.write(symbol + spaces);
-          };
-        });
-        process.stdout.write("\n");
-      };
-      offset++;
-    };
-  };
-};
-
-var args = process.argv.slice(2)
+const args = process.argv.slice(2)
 
 if (args.length > 0) {
-  var input;
-  var segments = 2;
-  if (args[0] == "-s") {
-    segments = args[1];
-    input = args[2];
+  let input
+  let segments = 2
+  if (args[0] === '-s') {
+    segments = args[1]
+    input = args[2]
   } else {
-    input = args[0];
+    input = args[0]
   };
-  var lcd = new LCD(input, segments);
-  lcd.draw();
+  const lcd = new LCD(input, segments)
+  lcd.draw()
 } else {
-  console.log("Usage: node lcd.js [-s SEGMENTS] NUMBER")
+  console.log('Usage: node lcd.js [-s SEGMENTS] NUMBER')
 };
